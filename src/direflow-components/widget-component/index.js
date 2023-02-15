1076927ee894db5b90abd2ddd5391f7045c6564f
Ozara Tsubasa <alcantaracloyd26@gmail.com>
2023-02-16 00:44:30 +0800

    update

--- src/direflow-components/widget-component/index.js
+++ src/direflow-components/widget-component/index.js
@@ -2,15 +2,18 @@ import { DireflowComponent } from 'direflow-component';
 import App from './App';
 
 const component = document.querySelector('widget-component');
-console.log('component main', component)
-console.log('component main', component.getAttribute('data-id'))
-console.log('component main 1', component.getAttribute('id'))
-console.log('component main 2', component.getAttribute('question'))
+// console.log('component main', component)
+const id = component.getAttribute('data-id')
+// console.log('component main 2', component.getAttribute('question'))
 
 export default DireflowComponent.create({
   component: App,
   configuration: {
     tagname: 'widget-component'
+  },
+  properties: {
+    componentTitle: 'Jobshot Widget',
+    dataId: id
   },
   plugins: [
     {
